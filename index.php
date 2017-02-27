9aa7b18bb56f30670be5f84c5c6c0999aa49c10c
Iurii Bell <ds@inbox.ru>
2017-02-28 02:09:11 +0300

    Composer files

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+	require_once __DIR__ . '/tests/bootstrap.php';
+
+
+
+	use PhpExcelForm\PhpExcelFormTests\SampleTest;
+
+	$helper = new \PhpOffice\PhpSpreadsheet\Helper\Sample();
+	if (!defined('EOL')) {
+	    define('EOL', $helper->isCli() ? PHP_EOL : '<br />');
+	}
+
+	// Return to the caller script when runs by CLI
+	if ($helper->isCli()) {
+	    return;
+	}
+
+
+
+	$test = new SampleTest();
+
+
+
+	$test->testSample(__DIR__.'/tests/data/SampleTest');
